49b661e1a769d2f6dd9a767e7c2dbef751bbea0d
dharmeshrao <yadavdydv@gmail.com>
2022-08-12 21:13:55 +0530

    2 files modified

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import { dbConnection } from './configs/db';
 import app from './index';
 
-const port: number = process.env.PORT || 4000;
+// const port: number = Number(process.env.PORT) || 4000;
+const randomPort = Array(4)
+  .fill(0)
+  .reduce(
+    (acc: string, curr: number) => acc + Math.floor(Math.random() * 10),
+    ''
+  );
 
-app.listen(port, async () => {
+app.listen(randomPort, async () => {
+  console.log('listning on port', randomPort);
   await dbConnection();
 });
--- src/utils/extrs.ts
+++ src/utils/extrs.ts
@@ -1,3 +1,28 @@
-import * as logger from 'winston';
+import * as winston from 'winston';
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: '.logs/error.log',
+    level: 'error',
+  }),
+  new winston.transports.File({ filename: '.logs/all.log' }),
+];
+
+const logger = winston.createLogger({
+  level: 'info',
+  levels,
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports,
+});
 
 export { logger };
