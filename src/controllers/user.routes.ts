2d7b27749baf9b6c25d24487bd1521916dc609bb
dharmeshrao <yadavdydv@gmail.com>
2022-08-12 18:33:19 +0530

    1 file modified

--- src/controllers/user.routes.ts
+++ src/controllers/user.routes.ts
@@ -2,6 +2,6 @@ import express, { Router } from 'express';
 import { userController } from './user.controller';
 const router: Router = express.Router();
 
-router.use('/login', userController.addCookiesAndPhoneNumber);
+router.post('/login', userController.addCookiesAndPhoneNumber);
 
 export default router;
