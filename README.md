52ec3b2d67b48e69c886b72c0c434be412282d03
dharmeshrao <yadavdydv@gmail.com>
2022-08-12 16:29:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-japx15
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-japx15)
---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express')();
+
+express.listen(4000, () => {
+  console.log('listing on port here what do you know about oit');
+});
