712eb3513c367a05e48588efe182880d6b89ad64
dharmeshrao <yadavdydv@gmail.com>
2022-08-12 20:38:16 +0530

    1 file modified

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { dbConnection } from './configs/db';
 import app from './index';
 
-const port = process.env.PORT || 4000;
+const port: number = process.env.PORT || 4000;
 
 app.listen(port, async () => {
   await dbConnection();
